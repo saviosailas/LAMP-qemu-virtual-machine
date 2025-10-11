a0559d112907a0d31efbb9ec7fea850bb46c7501
savio sailas <30656522+saviosailas@users.noreply.github.com>
2025-10-11 17:47:48 +0530

    install lamp script

--- vm-with-lamp-config/install.sh
+++ vm-with-lamp-config/install.sh
@@ -1,1 +1,20 @@
 apk add apache2 phpmyadmin php83-apache2 mariadb mariadb-client
+rc-update add apache2 default
+rc-update add mariadb default
+rc-service apache2 restart
+# Initialize MySQL Data Directory. 
+mariadb-install-db --user=mysql --datadir=/var/lib/mysql
+rc-service mariadb start
+mysql_secure_installation <<EOF
+> 
+> y
+> secret
+> secret
+> y
+> y
+> y
+> y
+> EOF
+
+mysql -u root -r "ALTER USER 'root'@'localhost' IDENTIFIED BT 'toor';"
+
